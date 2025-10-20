f77d75420f34254574c9dde0427c4f667be226e2
HyoYoonNam <dev.rudevico@gmail.com>
2025-10-20 13:43:22 +0900

    refactor(calculate): calculate 메서드의 파라미터 타입을 String에서 int[]로 변경
    테스트 메서드 calculate_user_input_with_basic_delimiters를 통과시키기 위해 파라미터 타입을 변경한다.
    
    요소 합 계산에 필요한 int[] separated는 파라미터가 아니라, 내부에서 separate 메서드를 호출해 얻도록 한다.

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -14,7 +14,9 @@ public class StringCalculator {
     private static final List<String> DELIMITERS = new ArrayList<>(BASIC_DELIMITERS);
     private static final String DECIMAL_REGEX_PATTERN = "0-9";
 
-    public static int calculate(int[] separated) {
+    public static int calculate(String userInput) {
+        int[] separated = separate(userInput);
+
         int sum = 0;
         for (int number : separated) {
             sum += number;
